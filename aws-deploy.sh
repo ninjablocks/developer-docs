342cdf11d427e3335fc3fccce4962d94b64f39dd
Theo Julienne <theo@ninjablocks.com>
2014-06-04 14:08:21 +1000

    Export the variable so sphinx can see it.

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf sphinx_ninja_theme
 git clone git@github.com:ninjablocks/sphinx_ninja_theme.git
-PYTHONPATH=`pwd`/sphinx_ninja_theme
+export PYTHONPATH=`pwd`/sphinx_ninja_theme
 
 make html
 s3cmd sync -P build/html/ s3://${BUCKET_NAME}
